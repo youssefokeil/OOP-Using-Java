9a50e0e09b3f7b80a4fb03bc0a89cf9736f06e74
Youssef Okeil <youssef_okeil@yahoo.com>
2024-05-06 19:20:44 +0300

    Multiple Stages

--- FirstAppFX/src/main/java/org/example/firstappfx/HelloApplication.java
+++ FirstAppFX/src/main/java/org/example/firstappfx/HelloApplication.java
@@ -3,6 +3,7 @@ package org.example.firstappfx;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -15,6 +16,12 @@ public class HelloApplication extends Application {
         stage.setTitle("Hello!");
         stage.setScene(scene);
         stage.show();
+
+        Stage secondaryStage = new Stage();
+        secondaryStage.setTitle("Secondary Stage");
+        secondaryStage.setScene(new Scene(new Button("Hello from the other world"),320,240));
+        secondaryStage.setResizable(false);
+        secondaryStage.show();
     }
 
     public static void main(String[] args) {
